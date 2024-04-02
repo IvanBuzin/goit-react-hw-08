0a69777ec4177db177bda7005af61592d522707b
IvanBuzin <ua.buzin@gmail.com>
2024-04-02 22:26:37 +0300

    sd

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,37 +3,31 @@ import css from "./Navigation.module.css";
 import clsx from "clsx";
 import { useSelector } from "react-redux";
 import { selectIsLogged } from "../../redux/auth/selectors";
-import LoggedUser from "../LoggedUser/LoggedUser";
-import RegistrationMenu from "../RegistrationMenu/RegistrationMenu";
 
 const Navigation = () => {
   const logIn = useSelector(selectIsLogged);
   return (
     <header className={css.head}>
-      <nav className={css.navigate}>
-        <ul className={css.listLink}>
-          <li>
-            <NavLink
-              className={({ isActive }) => {
-                return clsx(css.link, isActive && css.active);
-              }}
-              to="/"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) => {
-                return clsx(css.link, isActive && css.active);
-              }}
-              to="/contacts"
-            >
-              Contacts
-            </NavLink>
-          </li>
-        </ul>
-        {logIn ? <LoggedUser /> : <RegistrationMenu />}
+      <nav className={css.listLink}>
+        <NavLink
+          className={({ isActive }) => {
+            return clsx(css.link, isActive && css.active);
+          }}
+          to="/"
+        >
+          Home
+        </NavLink>
+
+        {logIn && (
+          <NavLink
+            className={({ isActive }) => {
+              return clsx(css.link, isActive && css.active);
+            }}
+            to="/contacts"
+          >
+            Contacts
+          </NavLink>
+        )}
       </nav>
     </header>
   );
