6e48b46d106810b50fdc4cb8ae0d6aac2e9b11d4
IvanBuzin <ua.buzin@gmail.com>
2024-04-08 21:32:38 +0300

    ssdfg

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,28 +1,16 @@
-import { useEffect } from "react";
-import { selectFilteredContacts } from "../../redux/contacts/selectors";
+import { selectFilteredContacts } from "../../redux/filters/selectors";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
-import { nanoid } from "nanoid";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchContacts } from "../../redux/contacts/operations";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 export const ContactList = () => {
   const filteredContacts = useSelector(selectFilteredContacts);
-  const dispatch = useDispatch();
-  const isloggedIn = useSelector(selectIsLoggedIn);
-
-  useEffect(() => {
-    if (isloggedIn) {
-      dispatch(fetchContacts());
-    }
-  }, [dispatch, isloggedIn]);
 
   return filteredContacts.length > 0 ? (
     <ul className={css.contactList}>
-      {filteredContacts.map((item) => (
-        <li key={nanoid()} className={css.contactListItem}>
-          <Contact contacts={item} />
+      {filteredContacts.map((contact) => (
+        <li key={contact.id} className={css.contactListItem}>
+          <Contact data={contact} />
         </li>
       ))}
     </ul>
