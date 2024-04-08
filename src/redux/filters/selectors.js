f8bd11059d262a3f7349316bc8e6940c39393f23
IvanBuzin <ua.buzin@gmail.com>
2024-04-08 21:44:07 +0300

    nhy

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,1 +1,12 @@
+import { createSelector } from "@mui/material/styles/createTransitions";
+import { selectContacts } from "../contacts/selectors";
+
 export const selectNameFilter = (state) => state.filters.name;
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, nemeFilter) => {
+    return contacts.filters((contact) =>
+      contact.name.toLowerCase().includes(nemeFilter.toLowerCase())
+    );
+  }
+);
