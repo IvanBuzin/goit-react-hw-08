8199cd6be08b80448dc254a2b08d7fb0fbfdad7a
IvanBuzin <ua.buzin@gmail.com>
2024-04-08 22:00:33 +0300

    vbn

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,22 +1,5 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { selectNameFilter } from "../filters/selectors";
+export const selectError = (state) => state.contacts.error;
 
 export const selectContacts = (state) => state.contacts.items;
 
 export const selectLoading = (state) => state.contact.loading;
-
-export const selectFilteredContacts = createSelector(
-  [selectContacts, selectNameFilter],
-  (contacts, filter) => {
-    return contacts.length > 0
-      ? contacts.filter((contact) =>
-          contact.name
-            .toLowerCase()
-            .includes(
-              filter.toLowerCase() &&
-                contact.number.toLowerCase().includes(filter)
-            )
-        )
-      : [];
-  }
-);
